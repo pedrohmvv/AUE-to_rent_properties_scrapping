b111b32f33a38c31f63c1597c04693d78bd0e771
Pedro Henrique Medeiros Vieira <pedro.vieira@dataprev.gov.br>
2024-12-18 10:25:27 -0300

    listing time added

--- src/scrapper.py
+++ src/scrapper.py
@@ -84,7 +84,7 @@ class Scrapper:
                 "Bedrooms": [self.extract_text(session.find_all('p', {'data-testid': self.config.vars.id_bedrooms_class}))],
                 "Bathrooms": [self.extract_text(session.find_all('p', {'data-testid': self.config.vars.id_bathroom_class}))],
                 "Area": [self.extract_text(session.find_all('p', {'data-testid': self.config.vars.id_area_class}))],
-                "Listing Time": [self.extract_text(session.find('p', {'data-testid': self.config.vars.id_time}))],
+                "Listing Time": [article.find('p', {'class': self.config.vars.id_time}).text],
                 "Link": [article.find('a', {'data-testid': 'property-card-link'})['href']],
                 "Contact": [article.find('a', {'data-testid': self.config.vars.call_id})['href']],
             }
